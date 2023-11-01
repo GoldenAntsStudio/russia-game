2e17a6faa063d0eaf017b4b93a6390648e15c0cb
Smart Ant Studio <hometikhon777@gmail.com>
2023-11-01 12:15:11 +0300

    1.1

--- db.py
+++ db.py
@@ -1,31 +1,52 @@
+#Импорт модулей
 import sqlite3
-#USER DB
-user_db = sqlite3.connect("user.db", check_same_thread=False)
-user_cur = user_db.cursor()
-#PROPERTY DB
-property_db = sqlite3.connect("property.db", check_same_thread=False)
-property_cur = property_db.cursor()
+
+db = sqlite3.connect("DataBase.db", check_same_thread=False)
+cur = db.cursor()
+
 
 def commit():
     print('COMMITING')
-    user_db.commit()
+    db.commit()
 
+#Проверка при ходе в аккаунт
+def login_db(nickname):
+    cur.execute('SELECT EXISTS (SELECT 1 FROM User WHERE nickname = ?)', (nickname,))
+    existUser = cur.fetchone()[0]
 
+    cur.execute('SELECT password FROM User WHERE nickname = ?', (nickname,))
+    password = cur.fetchall()[0][0]
+
+    return existUser, password
+    
+#Регистрация, запись данных нового игрока в базу данных
 def register_db(nickname, email, number, password):
-    accountBool = user_cur.execute('SELECT * FROM User WHERE nickname=?', (nickname,))
-    if accountBool.fetchone() is None:
-        user_cur.execute('INSERT INTO User (nickname, email, number, password) VALUES(?, ?, ?, ?)', (nickname, email, number, password))
+    
+    emailBool = cur.execute('SELECT email FROM User WHERE nickname=?', (email,))
+    
+    accountBool = cur.execute('SELECT * FROM User WHERE nickname=?', (nickname,))
+
+    if accountBool.fetchone() is None and emailBool.fetchone() is None:
+        cur.execute('INSERT INTO User (nickname, email, number, password) VALUES(?, ?, ?, ?)', (nickname, email, number, password))
+        cur.execute('INSERT INTO Property (rub, eur, usd) VALUES(?, ?, ?)', (250, 0, 0))
         commit()
     else:
-        return "НИК ЗАНЯТ"
+        return False
 
 
+#Отображение имущества и валют
 def account_card(id):
-    user_cur.execute('SELECT nickname FROM User WHERE id = ?', (id,))
-    nick = user_cur.fetchone()
+    cur.execute('SELECT nickname FROM User WHERE id = ?', (id,))
+    nick = cur.fetchone()
     nickname = nick[0]
+
+    cur.execute('SELECT rub, eur, usd FROM Property WHERE id = ?', (id,))
+    properties = cur.fetchall()
+    rub = properties[0][0]
+    usd = properties[0][1]
+    eur = properties[0][2]
     if nickname:
-        return nickname
+        return id, nickname, rub, usd, eur
     else:
         return 'Пользователь не найден'
     
--- main.py
+++ main.py
@@ -1,37 +1,52 @@
-from flask import Flask, request, render_template, session
-from db import register_db, account_card
+from flask import Flask, request, render_template, session, redirect, url_for
+from db import register_db, account_card, db, login_db
+from datetime import timedelta
 
 app = Flask(__name__)
-
-
+app.secret_key = 'Tikhon09091'
+app.permanent_session_lifetime = timedelta(days=10)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     
+    if request.method == "POST":
+        loginCheck = login_db(request.form['nickname'])
+
+        if loginCheck[0] == 1 and loginCheck[1] == request.form['password']:
+            user = request.form['nickname']
+            session["user"] = user
+            session['logged_in'] = True
+
+            return redirect('/me')
+        else:
+            return render_template("login.html", error='Логин или пароль неверный')
+    else:
+        return render_template("login.html", error='')
 
 
 
-    return render_template("login.html")
+@app.route("/me")
+def me():
+    if session.get('logged_in') == True:
+        return f'<p/>{session["user"]}<p>'
+    else:
+        return '<p/>Вы не вошли в аккаунт!<p>'
+        
 
-@app.route("/log", methods=['POST', 'GET'])
-def log():
-    nickname_form = request.form['nickname']
-    password_form = request.form['password']
 
-    session.new
-    return render_template("index.html")
+    
+@app.route("/work")
+def work():
+    if request.method == 'POST':
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
-
-@app.route("/reg", methods=['GET', 'POST'])
-def reg():
     if request.method == 'POST':
 
         nickname_form = request.form['nickname']
@@ -40,23 +55,38 @@ def reg():
         password_form = request.form['password']
 
         
-        if register_db(nickname_form, email_form, number_form, password_form) != 'НИК ЗАНЯТ':
+        if register_db(nickname_form, email_form, number_form, password_form) != False:
             register_db(nickname_form, email_form, number_form, password_form)
             return '<p/>Вы успешно зарегались<p>'
         else:
-            return '<p/>Ник занят<p>'
-
-        
+            return '<p/>Никнейм или почта заняты<p>'
     else:
-        print("ERROR")
+        return render_template("register.html")
+
 
 @app.route("/<int:id>")
 def account__card(id):
     
     if account_card(id):
-        return render_template('account_card.html', nickname = account_card(id))
+        nickname = account_card(id)[1]
+        rub = account_card(id)[2]
+        usd = account_card(id)[3]
+        eur = account_card(id)[4]
+
+        return render_template('account_card.html', 
+                               nickname = nickname,
+                               rub = rub,
+                               usd = usd,
+                               eur = eur
+                               )
+
     else:
         return '<p/>Пользователь не найден<p>'
 
+
+@app.errorhandler(404)
+def error(e):
+    return render_template('error.html'), 404
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=2020)
